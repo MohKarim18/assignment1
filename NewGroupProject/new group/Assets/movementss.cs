dcc30da2f966fd71a52fd9ff967c065298ab0c76
Md Redoy <rdy6792@gmail.com>
2019-09-09 11:19:49 -0400

    New empty Unity project.

---
+++ NewGroupProject/new group/Assets/movementss.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementss : MonoBehaviour
+{
+    public float movementSpeed = 0.001f;
+
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime;
+    }
+}
