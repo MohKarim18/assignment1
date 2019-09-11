dd19aec1432ffe9fe9ee60921288f6836853d89b
Md Redoy <rdy6792@gmail.com>
2019-09-10 22:06:29 -0400

    Update

---
+++ NewGroupProject/Assets/CubeMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public float speed = 1;
+    int randomCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomInteger();
+        Cube();
+        MoveScreen();
+    }
+
+    void RandomInteger()
+    {
+        randomCube = Random.Range(-10, 11);
+    }
+
+    void Cube()
+    {
+        transform.position = new Vector3(randomCube, transform.position.y, 0);
+    }
+
+    void MoveScreen()
+    {
+       
+        transform.position += Vector3.down * speed * Time.time;
+    }
+}
+
+
---
+++ NewGroupProject/Assets/CylinderMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderMovement : MonoBehaviour
+{
+
+    //float step = Mathf.PI / 60;
+    //float timeVar = 0;
+    //float rotationRange = 120;
+    //float baseDirection = 10;
+
+    //Vector3 randomDirection;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        transform.position = new Vector3(3 * Mathf.Sin(Time.time), 3 * Mathf.Cos(Time.time), 0);
+
+        //randomDirection = new Vector3(10, Mathf.Sin(timeVar) * (rotationRange / 2) + baseDirection, 0);
+        //transform.Rotate(randomDirection * Time.deltaTime * 10.0f);
+    }
+}
