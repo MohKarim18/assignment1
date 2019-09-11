7e0e521a42f2e7fc3d0395eb9650e763d1811d6b
Md Redoy <rdy6792@gmail.com>
2019-09-10 23:10:21 -0400

    UPdate final version

---
+++ NewGroupProject/Assets/CMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMovement : MonoBehaviour
+{
+    public float MovementSpeed = 0.001f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime;
+    }
+}
---
+++ NewGroupProject/Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float MovementSpeed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime;
+    }
+}
